a0f54a240371bfcacf1b24a07b23029a8ffeb7ca
Shlok | Cracksoft Securities <139556957+CracksoftShlok@users.noreply.github.com>
2024-04-08 15:12:25 +0530

    Calculate percentage of 5 Subject

---
+++ Task2.c
@@ -0,0 +1,8 @@
+//Write a code in C to Calculate percentage of 5 Subjects.
+#include <stdio.h>
+int main(){
+    float a=50,b=34,c=64,d=45,e=45,sum;
+    sum=a+b+c+d+e;
+    printf("Percentage of 5 Subject : %f",sum/5);
+    return 0;
+}
