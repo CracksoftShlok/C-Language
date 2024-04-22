bb47834bf9ab6129377538f9be6f300e72fe457c
Shlok | Cracksoft Securities <139556957+CracksoftShlok@users.noreply.github.com>
2024-04-22 21:02:42 +0530

    Percentage of 5 Subjects in C Lang

--- Task4.c
+++ Task4.c
@@ -4,5 +4,5 @@ int main(){
     float English=70,Physics=80,Chemistry=91.5,Maths=83,Computer=98,sum;
     sum=English+Physics+Chemistry+Maths+Computer;
     printf("%f",(sum/5));
-    
+    return 0;
 }
