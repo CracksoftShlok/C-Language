94f712ffc39e111445c3c43530acd55302516147
Shlok | Cracksoft Securities <139556957+CracksoftShlok@users.noreply.github.com>
2024-04-22 21:01:09 +0530

    Percentage of 5 Subject

---
+++ Task4.c
@@ -0,0 +1,8 @@
+//write a code in C Languge to print Percentage of 5 Subject.
+#include<stdio.h>
+int main(){
+    float English=70,Physics=80,Chemistry=91.5,Maths=83,Computer=98,sum;
+    sum=English+Physics+Chemistry+Maths+Computer;
+    printf("%f",(sum/5));
+    
+}
